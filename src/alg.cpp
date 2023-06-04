5201e8e24265151f4dc34f6e82dc7fcf560996e4
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-04 19:08:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,22 +3,34 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> BSTtree;
+  // поместите сюда свой код
+  std::string slovo;
   std::ifstream file(filename);
-  std::string otvet;
+  BST<std::string>* tree = new BST<std::string>;
+  char dlina = ' ';
   while (!file.eof()) {
-    char slovoobukva = file.get();
-    if (slovoobukva >= 'A' && slovoobukva <= 'Z')
-        slovoobukva = slovoobukva + ('a' - 'A');
-    if (slovoobukva >= 'a' && slovoobukva <= 'z') {
-      otvet = otvet + slovoobukva;
-    } else {
-      BSTtree.add(result);
-      otvet.clear();
+    while (dlina < 65 && (!file.eof())) {
+      file.get(str);
     }
+    while (dlina >= 65 && (!file.eof())) {
+      if (dlina >= 65 && dlina <= 90) {
+        slovo += dlina;
+      }
+      if (dlina >= 97 && dlina <= 122) {
+        slovo += dlina;
+      }
+      file.get(dlina);
+    }
+    for (int i = 0; i < slovo.length(); i++) {
+      if (slovo[i] >= 65 && w[i] <= 90)
+        slovo[i] += 32;
+    }
+    (*tree).add(slovo);
+    slovo = "";
   }
-  return BSTtree;
+  return *tree;
 }
