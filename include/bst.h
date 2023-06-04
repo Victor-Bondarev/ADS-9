8a1b21d4e744999a6cce3571cff7687299eb045e
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-04 19:02:23 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,7 +2,6 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 #include <algorithm>
-
 template <typename T>
 class BST {
  private:
@@ -13,29 +12,41 @@ class BST {
     };
     NODE* root;
     NODE* addNODE(NODE* root, const T& value) {
-        if (!root) {
-            root = new NODE{value, 1, nullptr, nullptr};
+        if (root == nullptr) {
+            root = new NODE;
+            root->value = value;
+            root->count = 1;
+            root->left = root->right = nullptr;
         } else if (root->value > value) {
             root->left = addNODE(root->left, value);
         } else if (root->value < value) {
             root->right = addNODE(root->right, value);
         } else {
-            ++root->count;
+            root->count += 1;
         }
         return root;
     }
     int heightTree(NODE* root) {
-        return root ? std::max(heightTree(root->left), heightTree(root->right)) + 1 : 0;
+        if (root == nullptr) return 0;
+        return std::max(heightTree(root->left), heightTree(root->right)) + 1;
     }
     NODE* searchTree(NODE* root, const T& value) {
-        return !root || root->value == value ? root : value < root->value ? searchTree(root->left, value) : searchTree(root->right, value);
+        if (root == nullptr || root->value == value) return root;
+        if (value < root->value) return searchTree(root->left, value);
+        return searchTree(root->right, value);
     }
 
  public:
     BST() : root(nullptr) {}
     void add(const T& value) { root = addNODE(root, value); }
     int depth() { return heightTree(root) - 1; }
-    int search(const T& value) { return searchTree(root, value) ? searchTree(root, value)->count : 0; }
+    int search(const T& value) {
+        NODE* current = searchTree(root, value);
+        if (current == nullptr) {
+            return 0;
+        } else {
+            return current->count;
+        }
+    }
 };
-
 #endif  // INCLUDE_BST_H_
